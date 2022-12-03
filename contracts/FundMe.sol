4f9de5c2b3d2a968bafcd79a60d9ca5b53db3a85
Abu Taher Md. Shifat <shifatatm@gmail.com>
2022-12-03 22:05:45 +0600

    creation of contracts

---
+++ contracts/FundMe.sol
@@ -0,0 +1,87 @@
+//Get Funds from Users
+//Withdraw funds
+//set a minimum funding value in USD
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "./PriceConverter.sol";
+
+error NotOwner(); // custom error
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 5 * 1e18; // 1 * 10 ** 18
+    //transaction cost
+    //837381 gas  = non-constant
+    //817827 gas = constant
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmount;
+    address public immutable i_owner;
+
+    //execution cost
+    // 817827 gas = non-immutable
+    // 794344 gas = immutable
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "Didn't send enough ether"
+        );
+
+        funders.push(msg.sender);
+        addressToAmount[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        //for loop
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+
+            addressToAmount[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        //transfer
+        //send
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call Failed");
+    }
+
+    modifier onlyOwner() {
+        //require(msg.sender == i_owner, "You are not the owner");
+        //794344 gas = without custom error
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        //769245 gas = with custom error
+        _;
+    }
+
+    //what happens if someone sends this contract without calling the fund function?
+
+    //receive()
+    //fallback()
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal view returns (uint256) {
+        //ABI
+        //Eth/USD
+        //	0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+
+        //price has 8 decimals
+        //Ether has 18 decimals as 1 eth = 1e18 wei
+        //10 additional 0 is to match the length
+
+        return uint256(price * 1e10);
+        // return uint256(price);
+    }
+
+    function getDecimals() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+        return priceFeed.decimals();
+    }
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+
+        return ethAmountInUsd;
+    }
+}
