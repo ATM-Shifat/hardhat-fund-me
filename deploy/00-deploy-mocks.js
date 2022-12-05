43ed012677e9e77cfabe2904dbb5274356cddab9
Abu Taher Md. Shifat <shifatatm@gmail.com>
2022-12-05 22:11:36 +0600

    Local deploy script added

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,28 @@
+//for usages in local network where data feed oracles are not available
+//hardhat network is an example
+
+const { network } = require("hardhat")
+const {
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER
+} = require("../helper-hardhat-config")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    if (developmentChains.includes(network.name)) {
+        log("Local network detected! Deploying mocks...")
+        await deploy("MockV3Aggregator", {
+            //contract: "MockV3Aggregator",
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_ANSWER]
+        })
+        log("Mocks deployed")
+        log("_________________________")
+    }
+}
+module.exports.tags = ["all", "mocks"]
